94424c5e2eb19e9fe16946eb4dc07a962d3b1ad9
Declan Brick <brickd@umich.edu>
2024-11-25 12:42:46 -0500

    Fix for heavy species update equation

--- include/Iteration.hpp
+++ include/Iteration.hpp
@@ -3,6 +3,6 @@
 
 namespace HypiC{
 
-    void Update_Heavy_Species(HypiC::Particles_Object Neutrals, HypiC::Particles_Object Ions, HypiC::Rate_Table_Object Ionization_Rates, double dt);
+    void Update_Heavy_Species(HypiC::Particles_Object Neutrals, HypiC::Particles_Object Ions, HypiC::Rate_Table_Object Ionization_Rates, HypiC::Options_Object Simulation_Parameters);
 
 }
