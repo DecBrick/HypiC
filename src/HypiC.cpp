643dddb9761e7580a7d502239966b46c971e1786
Declan Brick <brickd@umich.edu>
2024-11-25 12:04:16 -0500

    Small push to add outputs to the main loop

--- src/HypiC.cpp
+++ src/HypiC.cpp
@@ -9,13 +9,17 @@ void main(){
     HypiC::Options_Object Input_Options = HypiC::Options_Object();
     Input_Options.Read_Input("Input.txt");
 
-    //initialize
+    //initialize objects
     HypiC::Particles_Object Neutrals = HypiC::Initialize_Neutrals(Input_Options);
     HypiC::Particles_Object Ions = HypiC::Initialize_Ions(Input_Options);
     //Electrons = HypiC::Initialize_Electrons(Input_Options);
-    //HypiC::Time_Sum_Object Results = HypiC::Zero_Time_Sum();
-    HypiC::Rate_Table_Object Ionization_Rates = HypiC::Rate_Table_Object();
+
+    //initialize outputs 
+    HypiC::Time_Sum_Object Results = HypiC::Time_Sum_Object();
+    Results.Initialize_Time_Sum(Input_Options.nCells);
+    
     //read the ionization rates. 
+    HypiC::Rate_Table_Object Ionization_Rates = HypiC::Rate_Table_Object();
     //this assumes that the build directory is in the same folder as the HypiC directory, should enforce this.
     std::string file_path = "../../HypiC/Reactions/Xe_Ionization_0_to_1.txt";
     Ionization_Rates.Read_Table(file_path);
@@ -38,12 +42,12 @@ void main(){
         //Results = HypiC::Time_Sum(Neutrals, Ions, Electrons); 
 
         //periodic output
-        //if (i % Input_Options.Output_Interval == 0){
-        //    HypiC::Output(Results);
-       // }
+        if (i % Input_Options.Output_Interval == 0){
+            Results.Write_Output("Output.csv", Input_Options.nCells);
+        }
         
     }
 
     //final output
-    //HypiC::Output(Results);
+    Results.Write_Output("Output.csv", Input_Options.nCells);
 }
